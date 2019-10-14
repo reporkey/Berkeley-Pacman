b081b3b6f02a948b64e33298b9314a87b4bd54d1
reporkey <zhuzichunhogan@gmail.com>
2019-10-14 20:33:09 +1100

    add prediciton of enemy pos

--- pacman-contest/myTeam.py
+++ pacman-contest/myTeam.py
@@ -7,8 +7,9 @@ from util import nearestPoint
 import numpy as np
 from game import Grid
 
+
 def createTeam(firstIndex, secondIndex, isRed,
-               first='OffensiveReflexAgent', second='DefensiveReflexAgent'):
+               first='OffensiveVIAgent', second='DefensiveVIAgent'):
     """
     This function should return a list of two agents that will form the
     team, initialized using firstIndex and secondIndex as their agent
@@ -26,174 +27,138 @@ def createTeam(firstIndex, secondIndex, isRed,
     return [eval(first)(firstIndex), eval(second)(secondIndex)]
 
 
-class ReflexCaptureAgent(CaptureAgent):
-    """
-    A base class for reflex agents that chooses score-maximizing actions
-    """
+class ValueIterationAgent(CaptureAgent):
 
     def registerInitialState(self, gameState):
-        self.start = gameState.getAgentPosition(self.index)
+
         CaptureAgent.registerInitialState(self, gameState)
 
-    def chooseAction(self, gameState):
-        """
-        Picks among the actions with the highest Q(s,a).
-        """
-        actions = gameState.getLegalActions(self.index)
-
-        # You can profile your evaluation time by uncommenting these lines
-        # start = time.time()
-        values = [self.evaluate(gameState, a) for a in actions]
-        # print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
-
-        maxValue = max(values)
-        bestActions = [a for a, v in zip(actions, values) if v == maxValue]
-
-        foodLeft = len(self.getFood(gameState).asList())
-
-        if foodLeft <= 2:
-            bestDist = 9999
-            for action in actions:
-                successor = self.getSuccessor(gameState, action)
-                pos2 = successor.getAgentPosition(self.index)
-                dist = self.getMazeDistance(self.start, pos2)
-                if dist < bestDist:
-                    bestAction = action
-                    bestDist = dist
-            return bestAction
-
-        return random.choice(bestActions)
-
-    def getSuccessor(self, gameState, action):
-        """
-        Finds the next successor which is a grid position (location tuple).
-        """
-        successor = gameState.generateSuccessor(self.index, action)
-        pos = successor.getAgentState(self.index).getPosition()
-        if pos != nearestPoint(pos):
-            # Only half a grid position was covered
-            return successor.generateSuccessor(self.index, action)
-        else:
-            return successor
-
-    def evaluate(self, gameState, action):
-        """
-        Computes a linear combination of features and feature weights
-        """
-        features = self.getFeatures(gameState, action)
-        weights = self.getWeights(gameState, action)
-        return features * weights
-
-    def getFeatures(self, gameState, action):
-        """
-        Returns a counter of features for the state
-        """
-        features = util.Counter()
-        successor = self.getSuccessor(gameState, action)
-        features['successorScore'] = self.getScore(successor)
-        return features
-
-    def getWeights(self, gameState, action):
-        """
-        Normally, weights do not depend on the gamestate.  They can be either
-        a counter or a dictionary.
-        """
-        return {'successorScore': 1.0}
-class ValueIteration:
-
-    def __init__(self, gameState, index, epoch, heuristic, discount):
-        self.index = index
-        self.start = time.time()
-        self.isRed = gameState.isOnRedTeam(index)
-        self.discount = discount
         self.width, self.height = gameState.getWalls().width, gameState.getWalls().height
+        self.discount = 0.9
+        self.timeLimit = 0.8
+        self.heuristic = self.getHeuristic()
+
+        self.lastDefending = self.getFoodYouAreDefending(gameState).asList() + self.getCapsulesYouAreDefending(gameState)
+        self.enemiesPos = [gameState.getAgentPosition(i)
+                           if i in self.getOpponents(gameState) else None
+                           for i in range(4)]
+
+    def chooseAction(self, gameState):
+
+        # update enemy position info
+        self.updateEnemiesPos(gameState)
+
+        # define variable
+        self.start = time.time()
         self.rewards = np.zeros((self.width, self.height), dtype=None)
         self.Vs = np.zeros((self.width, self.height), dtype=None)
         self.policies = np.full((self.width, self.height), None)
         self.toUpdate = []
 
-        self.buildVMap(gameState, heuristic)
-        self.iteration(epoch)
+        # value iteration
+        self.buildVMap(gameState, self.getHeuristic())
+        self.iteration(self.timeLimit - 0.1)
         self.buildPoliciesMap()
 
+        (x, y) = gameState.getAgentPosition(self.index)
 
-    def buildVMap(self, gameState,heuristic):
+        return self.policies[x, y]
+
+    def buildVMap(self, gameState, heuristic):
         walls = gameState.getWalls().asList()
-        foods = gameState.getBlueFood().asList() if self.isRed else gameState.getRedFood().asList()
-        selffoods=gameState.getRedFood().asList() if self.isRed else gameState.getBlueFood().asList()
-        capsules = gameState.getBlueCapsules() if self.isRed else gameState.getRedCapsules()
-        width, height = gameState.getWalls().width, gameState.getWalls().height
+        food = self.getFood(gameState).asList()
+        defendingFood = self.getFoodYouAreDefending(gameState).asList()
+        capsules = self.getCapsules(gameState)
         numCarrying = gameState.getAgentState(self.index).numCarrying
-        deliveryLine = [(self.width // 2 - 1 if self.isRed else self.width // 2, y) for y in range(1, self.height)]
-
+        deliveryLine = [(self.width // 2 - 1 if self.red else self.width // 2, y) for y in range(1, self.height)]
 
         # build reward map
-        for x in range(width):  # set out boundary cell to None
+        for x in range(self.width):  # set out boundary cell to None
             self.rewards[x][0] = None
             self.rewards[x][-1] = None
-        for y in range(height):  # set out boundary cell to None
+        for y in range(self.height):  # set out boundary cell to None
             self.rewards[0][y] = None
             self.rewards[-1][y] = None
 
-        # evaluate heuristically
-        for (x, y) in foods:  # set reward of each food as 10
+        for (x, y) in food:
             self.rewards[x][y] += heuristic["food"]
-        for (x, y) in selffoods:  # set reward of each food as 10
-            if len(selffoods)<=8:
-              self.rewards[x][y] += heuristic["selffood"]
-            else:
-              self.rewards[x][y] =0
-        for (x, y) in capsules:  # set reward of each capsule as 100
-            self.rewards[x][y] += heuristic["capsule"]
-        for (x, y) in deliveryLine:
-            if len(foods)<=2:
-                self.rewards[x][y] += 5000 + heuristic["delivery"] * numCarrying #18 foods is enough, so  get back asap
-            else:
-                self.rewards[x][y] += heuristic["delivery"] * numCarrying
-        for (x, y) in walls:  # set reward of each WALL as None
-            self.rewards[x][y] = None
 
-        # lable visible enemies
-        enemyIndices = gameState.getBlueTeamIndices() if self.isRed else gameState.getRedTeamIndices()
-        for enemyIndex in enemyIndices:
-            enemyState = gameState.getAgentState(enemyIndex)
-            if enemyState.configuration is not None:
-                x, y = enemyState.getPosition()
+        # defending food
+        # for (x, y) in defendingFood:
+        #     if len(defendingFood) <= 8:
+        #         self.rewards[x][y] += heuristic["defendingFood"]
+        #     else:
+        #         self.rewards[x][y] = 0
+        #
+
+        for (x, y) in capsules:
+            self.rewards[x][y] += heuristic["capsule"]
+
+        for (x, y) in deliveryLine:
+            # 18 foods is enough, so  get back asap
+            # if len(food) <= 2:
+            #     self.rewards[x][y] += 5000 + heuristic["delivery"] * numCarrying
+            # else:
+            #
+            self.rewards[x][y] += heuristic["delivery"] * numCarrying
+
+        # label visible enemies
+        for index, pos in enumerate(self.enemiesPos):
+            if index in self.getOpponents(gameState) and pos is not None:
+
+                x, y = pos
+                enemyState = gameState.getAgentState(index)
+
+                # Enemy is pacman
                 if enemyState.isPacman:
+
+                    # I'm scared
                     if gameState.getAgentState(self.index).scaredTimer > 0:
                         self.rewards[int(x)][int(y)] += heuristic["enemyGhost"]
-                        #print(self.rewards[int(x)][int(y)])
+
+                    # I'm not scared
                     else:
                         self.rewards[int(x)][int(y)] += heuristic["enemyPacman"]
+
                 # I'm pacman, enemy is ghost
                 elif gameState.getAgentState(self.index).isPacman and not enemyState.isPacman:
-                    if enemyState.scaredTimer > 0:
-                        self.rewards[int(x)][int(y)] += heuristic["enemyPacman"]
-                    else:
-                        self.rewards[int(x)][int(y)] += (
-                        heuristic["enemyGhost"] + heuristic["foodLostPenalty"] * numCarrying)
+
+                    # Enemy is scared
+                    # if enemyState.scaredTimer > 0:
+                    #     self.rewards[int(x)][int(y)] += heuristic["enemyPacman"]
+                    #
+
+                    # Enemy is not scared
+                    if enemyState.scaredTimer < 3:
+                        self.rewards[int(x)][int(y)] += (heuristic["enemyGhost"] + heuristic["foodLostPenalty"] * numCarrying)
+
                 # I'm ghost, enemy is ghost
-                else:
-                    self.rewards[int(x)][int(y)] += heuristic["enemyPacman"]
+                # else:
+                #     self.rewards[int(x)][int(y)] += heuristic["enemyPacman"]
 
-        # TODO: assign "food delivery" reward based on number of its eaten; also higher penalty on ghost if eaten more
+        for (x, y) in walls:
+            self.rewards[x][y] = None
 
-        # list of position that required to be update during iterations
         self.toUpdate = [pos for pos, x in np.ndenumerate(self.rewards) if x == 0]
-        # print(self.toUpdate)
 
+    def iteration(self, timeLimit):
 
-    def iteration(self, epoch):
         self.Vs = self.rewards.copy()
 
-      # update all V values [epoch] times
-        for _ in range(epoch):
+        n = 0
+        # update all V values [epoch] times
+        while time.time() - self.start < timeLimit:
+
             oldVs = self.Vs.copy()
             for i, j in self.toUpdate:
                 self.Vs[i, j] = self.discount * max(self.getSuccessors(oldVs, i, j).values())
 
+            n += 1
+
+        print("iteration:", n)
 
     def buildPoliciesMap(self):
+
         # make up a policy map from V values
         for (x, y), value in np.ndenumerate(self.Vs):
             if not np.isnan(value):
@@ -214,6 +179,47 @@ class ValueIteration:
             else:
                 self.policies[x, y] = None
 
+    def updateEnemiesPos(self, gameState):
+
+        # predict pos by last eaten food/capsule
+        dist = lambda pos: self.getMazeDistance(gameState.getAgentPosition(self.index), pos)
+        enemiesDists = [dist(self.enemiesPos[i])
+                        if i in self.getOpponents(gameState)
+                           and self.enemiesPos[i] is not None
+                        else None
+                        for i in range(4)]
+        if enemiesDists.count(not None) > 0:
+            closedEnemyIndex = enemiesDists.index(min(x for x in enemiesDists if x is not None))
+        else:
+            closedEnemyIndex = self.getOpponents(gameState)[0]
+        defending = self.getFoodYouAreDefending(gameState).asList() + self.getCapsulesYouAreDefending(gameState)
+        for each in self.lastDefending:
+            if each not in defending:
+                self.enemiesPos[closedEnemyIndex] = each
+        self.lastDefending = defending
+
+        # observe pos by own vision
+        for i in self.getOpponents(gameState):
+            if gameState.getAgentState(i).getPosition() is not None:
+                self.enemiesPos[i] = gameState.getAgentState(i).getPosition()
+
+        # remove position of lost enemy when refreshing fog of war
+        myPos = gameState.getAgentPosition(self.index)
+        visibleEnemiesPos = []
+        for i in range(4):
+            if i in self.getOpponents(gameState):
+                if gameState.getAgentState(i).getPosition() is not None:
+                    visibleEnemiesPos.append(gameState.getAgentPosition(i))
+                else:
+                    visibleEnemiesPos.append(None)
+            else:
+                visibleEnemiesPos.append(None)
+        for i in self.getOpponents(gameState):
+            if self.enemiesPos[i] is not None \
+                    and distanceCalculator.manhattanDistance(myPos, self.enemiesPos[i]) < 5 \
+                    and self.enemiesPos[i] not in visibleEnemiesPos:
+                self.enemiesPos[i] = None
+
     def getSuccessors(self, grid, i, j):
         successors = {}  # successor = {(x, y) = V_value}
         if i - 1 >= 0 and not np.isnan(grid[i - 1, j]):
@@ -227,71 +233,38 @@ class ValueIteration:
 
         return successors
 
-
-class ValueiterationAgent(CaptureAgent):
-    def registerInitialState(self, gameState):
-        self.start = gameState.getAgentPosition(self.index)
-        CaptureAgent.registerInitialState(self, gameState)
-
-    def chooseAction(self, gameState):
-        valueIteration=ValueIteration(gameState, self.index, 150, self.getHeuristic(),0.9)
-        (x,y)=gameState.getAgentPosition(self.index)
-        return valueIteration.policies[x,y]
-
     def getHeuristic(self):
-
         """
         overwrite by subclass
         """
 
         features = util.Counter()
-        features['food'] = 1
-        features['capsule'] = 1
-        features['enemyGhost'] = -1
-        features['enemyPacman'] = 1
         return features
 
 
+class OffensiveVIAgent(ValueIterationAgent):
 
-class OffensiveReflexAgent(ValueiterationAgent):
-  """
-  A reflex agent that seeks food. This is an agent
-  we give you to get an idea of what an offensive agent might look like,
-  but it is by no means the best or only way to build an offensive agent.
-  """
-
-  def getHeuristic(self):
-      features = util.Counter()
-      features['food'] = 100
-      features['selffood'] =0
-      features['capsule'] = 200
-      features['delivery'] = 20
-      features['foodLostPenalty'] = -100
-      features['enemyGhost'] = -10000
-      features['enemyPacman'] = 50
-      return features
+    def getHeuristic(self):
+        features = util.Counter()
+        features['food'] = 100
+        features['defendingFood'] = 0
+        features['capsule'] = 200
+        features['delivery'] = 20
+        features['foodLostPenalty'] = -100
+        features['enemyGhost'] = -10000
+        features['enemyPacman'] = 50
+        return features
 
 
-class DefensiveReflexAgent(ValueiterationAgent):
-  """
-  A reflex agent that keeps its side Pacman-free. Again,
-  this is to give you an idea of what a defensive agent
-  could be like.  It is not the best or only way to make
-  such an agent.
-  """
-  def getHeuristic(self):
-      features = util.Counter()
-      features['food'] = 100
-      features['selffood'] = 20000
-      features['capsule'] = 0
-      features['delivery'] = 40
-      features['foodLostPenalty'] = -100
-      features['enemyGhost'] = -1000
-      features['enemyPacman'] = 50000
-      return features
+class DefensiveVIAgent(ValueIterationAgent):
 
-
-
-
-
-
+    def getHeuristic(self):
+        features = util.Counter()
+        features['food'] = 100
+        features['defendingFood'] = 20000
+        features['capsule'] = 0
+        features['delivery'] = 40
+        features['foodLostPenalty'] = -100
+        features['enemyGhost'] = -1000
+        features['enemyPacman'] = 50000
+        return features
